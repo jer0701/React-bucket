32001acdf0adf817e86ab0672051f34498acda27
yuanzhe <1018995917@qq.com>
2017-04-11 23:27:05 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
 
+import HomePage from './pages/Home'
 import UserAddPage from './pages/UserAdd';
 
 ReactDom.render((
     <Router history={hashHistory}>
+        <Route path="/" component={HomePage} />
         <Route path="/user/add" component={UserAddPage} />
     </Router>
     ), document.getElementById("app"));
