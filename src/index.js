9d82dbd99de86e1b8c562ec2ad3a58c8b0c17b5b
yuanzhe <1018995917@qq.com>
2017-04-11 23:16:55 +0800

    add router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+
+import UserAddPage from './pages/UserAdd';
 
 ReactDom.render((
-    <div>Hello React!</div>
+    <Router history={hashHistory}>
+        <Route path="/user/add" component={UserAddPage} />
+    </Router>
     ), document.getElementById("app"));
