4691f6c5c05771988acc45751f85c8c1c16f2b80
yuanzhe <1018995917@qq.com>
2017-04-26 17:56:47 +0800

    update server

--- server/auth.js
+++ server/auth.js
@@ -1,23 +1,22 @@
-const expireTime = 1000 * 60;
+const expireTime = 10000 * 60; //本地电脑时间有问题，所以设置10分钟比较久
 
 module.exports = function (req, res, next) {
-    res.header("Access-Control-Expose-Headers", "access-token");
-    const now = Date.now();
+  res.header('Access-Control-Expose-Headers', 'access-token');
+  const now = Date.now();
 
-    let unauthorized = true;
-    const token = req.header["access-token"];
-
-    if (token) {
-        const expired = now - token > expireTime;
-        if (!expired) {
-            unauthorized = false;
-            res.header("access-token", now);
-        }
+  let unauthorized = true;
+  const token = req.headers['access-token'];
+  if (token) {
+    const expired = now - token > expireTime;
+    if (!expired) {
+      unauthorized = false;
+      res.header('access-token', now);
     }
+  }
 
-    if (unauthorized) {
-        res.sendStatus(401);
-    } else {
-        next();
-    }
-}
+  if (unauthorized) {
+    res.sendStatus(401);
+  } else {
+    next();
+  }
+};
